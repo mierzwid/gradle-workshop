7b9995a8982c7759dbcaccf58278f251fbd25df6
mierzwid <mierzwid@gmail.com>
2019-07-17 10:17:20 +0200

    Exercise 9 - multi project - lib dependencies

--- exercise-09-lib-dependencies-ultimate/kotlin/build.gradle.kts
+++ exercise-09-lib-dependencies-ultimate/kotlin/build.gradle.kts
@@ -1,3 +1,8 @@
+// A “lib” dependency is a special form of an execution dependency. It causes the other project to be built first
+// and adds the jar with the classes of the other project to the classpath. It also adds the dependencies of the other
+// project to the classpath. So you can enter the “api” directory and trigger a “gradle compile”. First the “shared”
+// project is built and then the “api” project is built. Project dependencies enable partial multi-project builds.
+
 subprojects {
     apply(plugin = "java")
     group = "org.gradle.sample"
@@ -22,9 +27,3 @@ project(":services:personService") {
         "implementation"(project(":api"))
     }
 }
-
-
-// A “lib” dependency is a special form of an execution dependency. It causes the other project to be built first
-// and adds the jar with the classes of the other project to the classpath. It also adds the dependencies of the other
-// project to the classpath. So you can enter the “api” directory and trigger a “gradle compile”. First the “shared”
-// project is built and then the “api” project is built. Project dependencies enable partial multi-project builds.
